cb88f85d13ad7ad835b7cc6dfdf6ea23346336cf
brightonmboya <b.mboya@alustudent.com>
2024-08-15 16:02:35 +0300

    fixed the hover state on mobile

--- components/StanleyCarousel.tsx
+++ components/StanleyCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
 import "swiper/css";
@@ -42,44 +42,7 @@ export default function Page({ images }: any) {
         className="absolute top-0 left-0 w-full h-screen z-[-999]">
         {images.map((img, index) => (
           <SwiperSlide key={index}>
-            <div className="text-center h-full flex justify-center items-center group relative">
-              <Image
-                alt="Rafiki Pictures"
-                className="transform brightness-90 transition will-change-auto md:h-full md:object-bottom"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                blurDataURL={img.blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${img.public_id}.${img.format}`}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-              />
-
-              <div className="absolute transition-all duration-500 bg-gradient-to-t from-black/30 to-transparent group-hover:via-black/50 group-hover:ease-in-out ease-in-out w-full h-[40%]  bottom-0 flex items-center justify-center z-[999]">
-                <div className="absolute bottom-[5%] group-hover:bottom-[15%] transition-all duration-500">
-                  {/* <p className="text-white text-base lg:text-xl uppercase font-medium transition-all duration-700 px-6">
-                    {img.caption}
-                  </p> */}
-                  <p
-                    className="text-white text-base lg:text-xl uppercase font-medium transition-all duration-700 px-6"
-                    dangerouslySetInnerHTML={{ __html: img.caption }}
-                  />
-                  <div className="transition-all duration-150">
-                    <YoutubeModal
-                      width={img.video.width}
-                      height={img.video.height}
-                      className={img.video.className}
-                      url={img.video.url}
-                      videoType={img.videoType}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="bg-gradient-to-t from-black via-black/50 to-transparent w-full h-[30%] absolute left-0 bottom-0 " />
-            </div>
+            <HoverableImage img={img} />
           </SwiperSlide>
         ))}
         <SwiperNavButtons />
@@ -99,6 +62,60 @@ const SwiperNavButtons = () => {
       <div
         onClick={() => swiper.slideNext()}
         className="swiper-button-next after:scale-[0.40] w-fit bg-white px-6 rounded flex justify-center items-center"></div>
+    </div>
+  );
+};
+
+const HoverableImage = ({ img }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+
+  return (
+    <div
+      className="text-center h-full flex justify-center items-center group relative"
+      onTouchStart={() => setIsHovered(!isHovered)}
+      onTouchCancel={() => setIsHovered(false)}
+      >
+      <Image
+        alt="Rafiki Pictures"
+        className="transform brightness-90 transition will-change-auto h-full object-cover"
+        style={{ transform: "translate3d(0, 0, 0)" }}
+        placeholder="blur"
+        blurDataURL={img.blurDataUrl}
+        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${img.public_id}.${img.format}`}
+        width={720}
+        height={480}
+        sizes="(max-width: 640px) 100vw,
+          (max-width: 1280px) 50vw,
+          (max-width: 1536px) 33vw,
+          25vw"
+      />
+
+      <div
+        className={`absolute transition-all duration-500 bg-gradient-to-t from-black/30 to-transparent w-full h-[40%] bottom-0 flex items-center justify-center z-[999] ${
+          isHovered ? "group-hover:via-black/50" : ""
+        }`}>
+        <div
+          className={`absolute bottom-[5%] ${
+            isHovered ? "bottom-[30%]" : ""
+          } transition-all duration-500`}>
+          <p
+            className="text-white text-base lg:text-xl uppercase font-medium transition-all duration-700 px-6"
+            dangerouslySetInnerHTML={{ __html: img.caption }}
+          />
+          <div className="transition-all duration-150">
+            <YoutubeModal
+              width={img.video.width}
+              height={img.video.height}
+              className={img.video.className}
+              url={img.video.url}
+              videoType={img.videoType}
+              isHovered={isHovered}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-gradient-to-t from-black via-black/50 to-transparent w-full h-[30%] absolute left-0 bottom-0 " />
     </div>
   );
 };
--- components/ui/youtubeModal.tsx
+++ components/ui/youtubeModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -13,6 +14,7 @@ interface VideoProps {
   url: string;
   className: string;
   videoType: string;
+  isHovered: boolean;
 }
 
 export default function YoutubeModal({
@@ -21,19 +23,39 @@ export default function YoutubeModal({
   url,
   videoType,
   className,
+  isHovered,
 }: VideoProps) {
   const isPortrait = videoType === "portrait";
-  const modalWidth = isPortrait ? "w-[400px]" : "min-w-[80%]";
-  const minWidth = isPortrait ? "w-[30%]" : "w-[80%]"
+  const modalWidth = isPortrait ? "w-[300px] md:w-[400px]" : "min-w-[80%]";
+  const minWidth = isPortrait ? "w-[20%] md:w-[30%]" : "w-[80%]";
   const iframeWidth = isPortrait ? 280 : width;
   const iframeHeight = isPortrait ? 500 : height;
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <button
-          className={`relative py-1 px-6 w-full font-semibold text-lg text-center flex items-center justify-center mx-auto sm:w-fit hover:before:bg-[#ff7900] overflow-hidden bg-white text-[#ff7900] transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#ff7900] before:transition-all before:duration-500 hover:text-white  hover:before:left-0 hover:before:w-full translate-y-10 group-hover:translate-y-4 opacity-0 group-hover:opacity-100 delay-300 duration-500`}>
-          <span className="relative z-10 font-acumin">WATCH VIDEO</span>
-        </button>
+        <>
+          <button
+            className={`relative py-1 px-6 w-full font-semibold text-lg text-center md:flex items-center justify-center mx-auto sm:w-fit
+               hover:before:bg-[#ff7900] overflow-hidden bg-white text-[#ff7900] transition-all before:absolute before:bottom-0 
+               before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-[#ff7900] before:transition-all before:duration-500
+                hover:text-white  hover:before:left-0 hover:before:w-full translate-y-10 group-hover:translate-y-[-5px] opacity-0 group-hover:opacity-100 
+                delay-300 duration-500 hidden `}
+            onClick={() => setIsOpen(true)}>
+            <span className="relative z-10 font-acumin">WATCH VIDEO</span>
+          </button>
+
+          <button
+            className={`${
+              isHovered && "bg-[#ff7900] text-white px-4 py-2"
+            } md:hidden`}
+            onClick={() => {
+              setIsOpen(true);
+            }}>
+            <span className="relative z-10 font-acumin">WATCH VIDEO</span>
+          </button>
+        </>
+        {/* <button className={`${isHovered && "bg-red-400"}`}>Hello world</button> */}
       </DialogTrigger>
       <DialogContent
         className={`p-2 md:p-0 overflow-hidden ${modalWidth} h-[80%] bg-black/40`}>
@@ -49,5 +71,3 @@ export default function YoutubeModal({
     </Dialog>
   );
 }
-
-  
